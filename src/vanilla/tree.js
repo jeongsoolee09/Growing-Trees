a1aaf781c0420ef017e07eda2c60801f4b1982a4
이정수 <jslee@ijeongsus-MacBook-Pro.local>
2022-02-19 21:21:21 +0900

    formatting (using ts-ls)

--- src/vanilla/tree.js
+++ src/vanilla/tree.js
@@ -1,77 +1,70 @@
 import { Branch } from './branch.js';
 
 export class Tree {
-	constructor(ctx, posX, posY) {
-		this.ctx = ctx;
-		this.posX = posX;
-		this.posY = posY;
-		this.branches = [];
-		this.depth = 11;
+    constructor(ctx, posX, posY) {
+        this.ctx = ctx;
+        this.posX = posX;
+        this.posY = posY;
+        this.branches = [];
+        this.depth = 11;
 
-		this.cntDepth = 0; // depth별로 그리기 위해 현재 depth 변수 선언
-		this.animation = null; // 현재 동작하는 애니메이션
+        this.cntDepth = 0; // depth별로 그리기 위해 현재 depth 변수 선언
+        this.animation = null; // 현재 동작하는 애니메이션
 
-		this.init();
-	}
+        this.init();
+    }
 
-	init() {
-		// depth별로 가지를 저장하기 위해 branches에 depth만큼 빈배열 추가
-		for (let i = 0; i < this.depth; i++) {
-			this.branches.push([]);
-		}
+    init() {
+        // depth별로 가지를 저장하기 위해 branches에 depth만큼 빈배열 추가
+        for (let i = 0; i < this.depth; i++) {
+            this.branches.push([]);
+        }
 
-		this.createBranch(this.posX, this.posY, -90, 0);
-		this.draw();
-	}
+        this.createBranch(this.posX, this.posY, -90, 0);
+        this.draw();
+    }
 
-	randomInt(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min) + min);
-	}
+    randomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
-	cos(x) {
-		return Math.cos(x)
-	}
-	sin(x) {
-		return Math.sin(x)
-	}
+    createBranch(startX, startY, angle, depth) {
+        if (depth === this.depth) return;
 
-	createBranch(startX, startY, angle, depth) {
-		if (depth === this.depth) return;
+        const len = depth === 0 ? this.randomInt(10, 13) : this.randomInt(0, 11);
 
-		const len = depth === 0 ? this.randomInt(10, 13) : this.randomInt(0, 11);
+        const endX = startX + Math.cos(angle) * len * (this.depth - depth);
+        const endY = startY + Math.sin(angle) * len * (this.depth - depth);
 
-		const endX = startX + Math.cos(angle) * len * (this.depth - depth);
-		const endY = startY + Math.sin(angle) * len * (this.depth - depth);
+        // depth에 해당하는 위치의 배열에 가지를 추가
+        this.branches[depth].push(
+            new Branch(startX, startY, endX, endY, this.depth - depth)
+        );
 
-		// depth에 해당하는 위치의 배열에 가지를 추가
-		this.branches[depth].push(
-			new Branch(startX, startY, endX, endY, this.depth - depth)
-		);
+        this.createBranch(endX, endY, angle - this.randomInt(15, 23), depth + 1);
+        this.createBranch(endX, endY, angle + this.randomInt(15, 23), depth + 1);
+    }
 
-		this.createBranch(endX, endY, angle - this.randomInt(15, 23), depth + 1);
-		this.createBranch(endX, endY, angle + this.randomInt(15, 23), depth + 1);
-	}
+    draw() {
+        // 다 그렸으면 requestAnimationFrame을 중단해 메모리 누수가 없게 함.
+        if (this.cntDepth === this.depth) {
+            cancelAnimationFrame(this.animation);
+        }
 
-	draw() {
-		// 다 그렸으면 requestAnimationFrame을 중단해 메모리 누수가 없게 함.
-		if (this.cntDepth === this.depth) {
-			cancelAnimationFrame(this.animation);
-		}
+        // depth별로 가지를 그리기
+        for (let i = this.cntDepth; i < this.branches.length; i++) {
+            let pass = true;
 
-		// depth별로 가지를 그리기
-		for (let i = this.cntDepth; i < this.branches.length; i++) {
-			let pass = true;
+            for (let j = 0; j < this.branches[i].length; j++) {
+                pass = this.branches[i][j].draw(this.ctx);
+            }
 
-			for (let j = 0; j < this.branches[i].length; j++) {
-				pass = this.branches[i][j].draw(this.ctx);
-			}
+            if (!pass) break;
+            this.cntDepth++;
+        }
 
-			if (!pass) break;
-			this.cntDepth++;
-		}
-
-		this.animation = requestAnimationFrame(this.draw.bind(this));
-	}
+        this.animation = requestAnimationFrame(this.draw.bind(this));
+    }
 }
